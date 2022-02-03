05601078d311474195a6ab6e1ba61a6c6c13e42c
Seth McCombs <seth@seth-mccombs.com>
2022-02-02 21:26:10 -0800

    spanish language

--- hello_test.go
+++ hello_test.go
@@ -10,15 +10,21 @@ func TestHello(t *testing.T) {
 		}
 	}
 
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 
 	})
